ac6d8c731c1fa70219b44f4862deb743053110d8
Hemanthburra <102217798+Hemanthburra@users.noreply.github.com>
2022-10-13 21:51:58 +0530

    #include <iostream> #include<cmath> #include<bits/stdc++.h> using namespace std;
    int main()
    {
      int a,b,c; 
        cin>>a>>b>>c;
    
        float s=(float)(a+b+c)/2; 
    
        float area=sqrt(s*(s-a)*(s-b)*(s-c)); 
    
        cout<<fixed << setprecision(2) <<area; 
    
        return 0;
    
    }

---
+++ Area_of_Triangle_.c
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<cmath> 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int a,b,c; 
+    cin>>a>>b>>c;
+
+    float s=(float)(a+b+c)/2; 
+
+    float area=sqrt(s*(s-a)*(s-b)*(s-c)); 
+
+    cout<<fixed << setprecision(2) <<area; 
+
+    return 0;
+
+}
