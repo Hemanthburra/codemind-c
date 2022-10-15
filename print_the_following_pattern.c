1730a83282708815d2ebc1709684f48c59bd7605
Hemanthburra <102217798+Hemanthburra@users.noreply.github.com>
2022-10-15 23:33:54 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int n,a=0,b=1;     cin>>n;     for(int i=1;i<=n;i++){         for (int j=n;j>=1;j--) cout<<j<<" ";         cout<<endl;     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,12 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n;
+    int n,a=0,b=1;
     cin>>n;
-    for (int i=1;i<=n;i++){
-        for (int j=1;j<=n;j++){
-            cout<<char(64+i)<<" ";
-        }
+    for(int i=1;i<=n;i++){
+        for (int j=n;j>=1;j--) cout<<j<<" ";
         cout<<endl;
     }
 }
